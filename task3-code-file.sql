e8b63d58e7857584f46b7cdb55e61e13904a603a
Ankit Singh <143281507+TheScratchCoder@users.noreply.github.com>
2025-06-28 18:52:49 +0530

    Add files via upload

---
+++ task3-code-file.sql
@@ -0,0 +1,94 @@
+
+
+-- Table: Employees
+
+CREATE TABLE Employees (
+    EmployeeID INT,
+    FirstName VARCHAR(50),
+    LastName VARCHAR(50),
+    Department VARCHAR(50),
+    Salary DECIMAL(10, 2),
+    JoiningDate DATE
+);
+
+
+-- 1. Use SELECT * and Specific Columns
+
+
+-- Select all columns
+
+SELECT * FROM Employees;
+
+-- Select specific columns
+
+SELECT FirstName, LastName, Salary FROM Employees;
+
+
+--  2. Apply WHERE, AND, OR, LIKE, BETWEEN
+
+
+-- Use WHERE to filter by department
+
+SELECT * FROM Employees
+WHERE Department = 'Engineering';
+
+-- Use WHERE with AND to filter multiple conditions
+
+SELECT * FROM Employees
+WHERE Department = 'Engineering' AND Salary > 70000;
+
+-- Use WHERE with OR
+
+SELECT * FROM Employees
+WHERE Department = 'HR' OR Department = 'Marketing';
+
+-- Use LIKE to find names starting with 'J'
+
+SELECT * FROM Employees
+WHERE FirstName LIKE 'J%';
+
+-- Use BETWEEN to find salaries in a range
+
+SELECT * FROM Employees
+WHERE Salary BETWEEN 60000 AND 80000;
+
+
+
+-- 3. Sort with ORDER BY
+
+
+-- Sort by Salary ascending
+
+SELECT * FROM Employees
+ORDER BY Salary ASC;
+
+-- Sort by JoiningDate descending
+
+SELECT * FROM Employees
+ORDER BY JoiningDate DESC;
+
+-- Combine WHERE and ORDER BY
+
+SELECT FirstName, Department, Salary
+FROM Employees
+WHERE Salary > 60000
+ORDER BY Salary DESC;
+
+
+
+-- Extract Data from Multiple Tables
+-- another table -  Departments
+
+CREATE TABLE Departments (
+    DepartmentName VARCHAR(50),
+    Manager VARCHAR(50)
+);
+
+-- joining both:
+
+SELECT E.FirstName, E.LastName, E.Department, D.Manager
+FROM Employees E
+JOIN Departments D ON E.Department = D.DepartmentName;
+
+
+
